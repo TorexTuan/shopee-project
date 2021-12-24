e694ec982cc77c774108305a9829c26e1eab5c87
TorexTuan <kanetuan4444@gmail.com>
2021-12-24 12:11:41 +0700

    edit port

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const session = require('express-session')
 const passport = require('passport')
 const flash = require('connect-flash')
 const passportStrategy = require('./config/passport')
-const port = 3000
+const port = process.env.PORT || 3000;
 
 // Overriding method
 app.use(methodOverride('_method'))
