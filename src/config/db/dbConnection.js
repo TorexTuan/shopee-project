3fe1d69f66a7e0ba31a7f4e468e8ee7a34813d0d
TorexTuan <kanetuan4444@gmail.com>
2021-12-24 15:31:46 +0700

    add useNewUrlParser into file

--- src/config/db/dbConnection.js
+++ src/config/db/dbConnection.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 async function dbConnection() {
 
     try {
-        await mongoose.connect('mongodb://localhost:27017/shopee_clone')
+        await mongoose.connect('mongodb://localhost:27017/shopee_clone', {
+            useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true 
+        })
         console.log('connect successfully...')
         
     } catch (error) {
