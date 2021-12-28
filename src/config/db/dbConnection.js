6591aeab9101e82a9b60aea6832c1c83e5678cdc
TorexTuan <kanetuan4444@gmail.com>
2021-12-28 21:28:09 +0700

    repush shopee project

--- src/config/db/dbConnection.js
+++ src/config/db/dbConnection.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 async function dbConnection() {
 
     try {
-        await mongoose.connect('mongodb://localhost:27017/shopee_clone', {
-            useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true 
-        })
+        await mongoose.connect('mongodb://localhost:27017/shopee_clone')
         console.log('connect successfully...')
         
     } catch (error) {
